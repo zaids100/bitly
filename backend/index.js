de073e9c5d58090715dde286509b2f0d82dc1e5b
zaids100 <zaidshaikh2619@gmail.com>
2024-12-19 02:51:20 +0530

    deployment success

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use(cors());
 app.use(cors({
-    origin : ["https://bitly-one.vercel.app"],
+    origin : ["https://bitly-one.vercel.app","https://bitly-captain-calculus-projects.vercel.app"],
     methods : ["GET","POST"],
     credentials : true
 }));  // Cross-Origin Resource Sharing middleware
