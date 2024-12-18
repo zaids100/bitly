62f7d976c030eb2a9e77d59a1228143623ba485f
zaids100 <zaidshaikh2619@gmail.com>
2024-12-19 02:32:14 +0530

    Deployment Setup

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,12 @@ dbConnect();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+    origin : ["https://bitly-one.vercel.app"],
+    methods : ["GET","POST"],
+    credentials : true
+}));  // Cross-Origin Resource Sharing middleware
 
 app.use('/',urlRoute)
 
