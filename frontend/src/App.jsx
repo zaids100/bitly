d181c5686f1879fdba35212c8c944bb1b9c486c4
Ritesh-Borale <riteshborale7@gmail.com>
2024-12-19 01:57:59 +0530

    Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,17 @@
-import React from 'react';
-import ReactTypingEffect from 'react-typing-effect'; 
+import React, { useState } from 'react';
+import ReactTypingEffect from 'react-typing-effect';
 import Header from './Components/Header';
 
 function App() {
+  const [link, setLink] = useState("https://shortened.url/12345");
+
+  const copyLink = () => {
+    navigator.clipboard.writeText(link).then(() => {
+    }).catch(err => {
+      console.error("Failed to copy: ", err);
+    });
+  };
+
   return (
     <div className="h-screen w-full bg-gradient-to-b from-gray-800 to-black overflow-hidden">
       <Header />
@@ -15,7 +24,7 @@ function App() {
             typingDelay={500}
             eraseSpeed={50}
             cursor="!"
-          /> 
+          />
         </h1>
 
         <div className="bg-gray-900 mt-8 px-8 py-6 rounded-xl shadow-xl max-w-4xl w-full">
@@ -29,7 +38,7 @@ function App() {
               placeholder="Enter URL here"
               className="p-4 bg-gray-800 text-gray-200 border border-gray-700 rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-blue-400 w-full sm:w-[830px] transition-all"
             />
-            
+
             <button className="w-72 bg-gradient-to-r from-blue-500 to-teal-500 text-white my-3 py-3 px-6 rounded-lg shadow-md hover:from-blue-600 hover:to-teal-600 transition-all flex items-center justify-center gap-2">
               <span>Get Your Free Link Here</span>
               <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -45,7 +54,25 @@ function App() {
             {/* <p className="text-red-500">Invalid URL</p> */}
           </div>
         </div>
+
+        <div className="bg-gradient-to-r from-gray-800 to-black-700 mt-5 px-8 py-6 rounded-2xl shadow-xl max-w-4xl w-full flex items-center justify-between">
+          <div className="text-gray-300 text-lg font-medium max-w-md overflow-hidden text-ellipsis" id="link-text">
+            {link}
+          </div>
+          <button
+            className="bg-gradient-to-r from-blue-500 to-teal-500 text-white py-3 px-8 rounded-full shadow-lg hover:bg-gradient-to-r hover:from-blue-600 hover:to-teal-600 transition-all ease-in-out duration-200 flex items-center gap-2"
+            onClick={copyLink}
+          >
+            <span className="text-lg">Copy Link!</span>
+            <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 7l5 5m0 0l-5 5m5-5H6"></path>
+            </svg>
+          </button>
+        </div>
+
+
       </div>
+
     </div>
   );
 }
